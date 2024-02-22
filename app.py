ca39b5b119dad8fc67f79b7eb4d593186df3fbc2
abdullah faqih muzakki <abdullahfaqihmuzakki@gmail.com>
2024-02-22 08:31:34 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'gwgatausecretkeybuatapa'
+
+db = SQLAlchemy(app)
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+class Events(db.Model):
+    name = db.Column(db.String, primary_key=True)
+    desc = db.Column(db.String)
+    time = db.Column(db.DateTime)
+    capacity = db.Column(db.Integer)
+    registered = db.Column(db.Integer)
+
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+@app.route("/")
+def home():
+    return render_template("/home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
